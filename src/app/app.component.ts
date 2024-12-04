18083144ace4eb35c2524a10c2e111dc23780863
GigiGiorgadze10 <giorgadze.gigi2004@gmail.com>
2024-12-04 17:53:15 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
