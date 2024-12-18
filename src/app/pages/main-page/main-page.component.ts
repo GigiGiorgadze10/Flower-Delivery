5e8cea09523e2dc52db16c91fbdd417f87957463
GigiGiorgadze10 <giorgadze.gigi2004@gmail.com>
2024-12-18 15:32:56 +0400

    changes

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../../components/hero/hero.component';
+import { InfoComponent } from '../../components/info/info.component';
+import { ServicesComponent } from '../../components/services/services.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent, InfoComponent, ServicesComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
