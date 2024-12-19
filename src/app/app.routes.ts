db91a64280d498041b69da27fd863255ece72607
GigiGiorgadze10 <giorgadze.gigi2004@gmail.com>
2024-12-19 14:38:11 +0400

    add sign in sign up forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
-import { provideRouter, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 export const routes: Routes = [
-    {path: '', component: MainPageComponent}
+    {path: '', component: MainPageComponent},
+    {path: 'signin', component: LoginComponent}
 ];
 
