6445a757902f94ee62008780d05e68b12c71e4a2
GigiGiorgadze10 <giorgadze.gigi2004@gmail.com>
2024-12-21 18:10:09 +0400

    add category page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CategoryComponent } from './pages/category/category.component';
 
 
 export const routes: Routes = [
     {path: '', component: MainPageComponent},
-    {path: 'signin', component: LoginComponent}
+    {path: 'signin', component: LoginComponent},
+    { path: 'category', component: CategoryComponent },
 ];
 
