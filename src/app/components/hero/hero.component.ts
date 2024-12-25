a78adf9f347813f7b66e0b136d68a75f52aa2b12
GigiGiorgadze10 <giorgadze.gigi2004@gmail.com>
2024-12-26 00:15:45 +0400

    delete share data service

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedDataService } from '../../services/shared-data.service';
 
 @Component({
   selector: 'app-hero',
@@ -10,9 +9,6 @@ import { SharedDataService } from '../../services/shared-data.service';
   styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent {
-  constructor(private sharedDataService: SharedDataService) {}
 
-  onSelectImage(image: string) {
-    this.sharedDataService.setSelectedImage(image);  // Emit the selected image
-  }
+  
 }
